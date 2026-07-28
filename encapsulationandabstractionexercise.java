35e112d806501de883ea891fa3fe306046257350
cooooolmeeeee04-tech <cooooolmeeeee04@gmail.com>
2026-07-28 15:08:22 +0800

    fifth comm

--- encapsulationandabstractionexercise.java
+++ encapsulationandabstractionexercise.java
@@ -1,25 +1,7 @@
 import java.util.*;
 
 
-class Van extends Vehicle {
-    private int cargoCapacity;
 
-    public Van(String plateNumber, String model, double baseRatePerDay, int cargoCapacity) {
-        super(plateNumber, model, baseRatePerDay);
-        this.cargoCapacity = cargoCapacity;
-    }
-
-    @Override
-    public double calculateRentalCost(int days) {
-        return (getBaseRatePerDay() * days) + 500; // driver fee
-    }
-
-    @Override
-    public void displayDetails() {
-        System.out.printf("Van | Plate: %s | Model: %s | Rate: %.2f | Capacity: %dkg | Available: %s%n",
-                getPlateNumber(), getModel(), getBaseRatePerDay(), cargoCapacity, isAvailable() ? "Yes" : "No");
-    }
-}
 
 class RentalSystem {
     private ArrayList<Vehicle> vehicles = new ArrayList<>();
