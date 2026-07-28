fac52665f96ff468eb0225410848d7bffdd74015
cooooolmeeeee04-tech <cooooolmeeeee04@gmail.com>
2026-07-28 15:04:43 +0800

    second comm

--- encapsulationandabstractionexercise.java
+++ encapsulationandabstractionexercise.java
@@ -1,29 +1,5 @@
 import java.util.*;
 
-abstract class Vehicle {
-    private String plateNumber;
-    private String model;
-    private double baseRatePerDay;
-    private boolean available;
-
-    public Vehicle(String plateNumber, String model, double baseRatePerDay) {
-        this.plateNumber = plateNumber;
-        this.model = model;
-        this.baseRatePerDay = baseRatePerDay;
-        this.available = true;
-    }
-
-    public String getPlateNumber() { return plateNumber; }
-    public String getModel() { return model; }
-    public double getBaseRatePerDay() { return baseRatePerDay; }
-    public boolean isAvailable() { return available; }
-    public void setAvailable(boolean available) { this.available = available; }
-
-    public abstract double calculateRentalCost(int days);
-
-    public abstract void displayDetails();
-}
-
 class Car extends Vehicle {
     private int seats;
 
